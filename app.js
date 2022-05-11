5306af100d552a1a3961371ba79f8a1af9ada56b
Ehiksalex <ehiksalex@gmail.com>
2022-05-11 23:42:15 +0100

    updated

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
+let score = document.getElementById('player1');
+let score1 = document.getElementById('player2');
+
+dice1 = 0;
+dice2 = 0;
 
 document.getElementById('TRY').addEventListener('click', () =>{
+    
+   
+   
+   
 
 var play = document.getElementById("play");
 
@@ -20,25 +29,41 @@ function rollDice(){
 //Using the IF and ELSE IF function to check for the winners
 
     if(firstDice > secondDice){
+        dice1 ++;
+        score.innerHTML = dice1;
+        score + 1;
         return "Player 1 wins!";
+       
     
     }
     else if (firstDice < secondDice){
+        dice2 ++;
+        score1.innerHTML = dice2;
+        score1 +1
         return "player 2 wins!";
        
+        
     }
     else{
+        score + 1;
+        score1 + 1
         return "It's a draw!";
-      
+    
     }
 }
 
 var playGame = rollDice();
 play.innerHTML = playGame;
+
 })
 
 //To Reset The Game
 
 document.getElementById('reset').addEventListener('click', () =>{
-  
+    dice1 = 0;
+    dice2 = 0;
+    score.innerHTML = dice1;
+    score1.innerHTML = dice2;
+
+    
 })
